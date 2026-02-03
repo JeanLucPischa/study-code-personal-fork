c740980f9ee8e72e5ed9c459382b7bc9a571e348
Jean-Luc Pischa <pischa.jean-lu-it25@it.dhbw-ravensburg.de>
2026-02-03 11:03:04 +0100

    minor changes

--- challenges/swap2dimensionalArray.c
+++ challenges/swap2dimensionalArray.c
@@ -1,57 +1,38 @@
 #include <stdio.h>
 
-int main(){
+void printArray(int array[][2]){
+  for (int i = 0; i < 2; i++){        //rows
+    for(int j = 0; j < 2; j++){       //columns
+      printf("%d ", array[i][j]);
+    }
+    printf("\n");
+  }
+}
 
+void swapArrays(int array1[][2], int array2[][2]){
+  for (int i = 0; i < 2; i++){        //rows
+    for(int j = 0; j < 2; j++){       //columns
+      int temporary = array1[i][j];
+      array1[i][j] = array2[i][j];
+      array2[i][j] = temporary;
+    }
+  }
+}
+
+int main(){
+    
   int arr1[2][2] = {{1, 2}, {3, 4}};
   int arr2[2][2] = {{5, 6}, {7, 8}};
 
   printf("Current content of arr1 and arr2:\n");
+  printArray(arr1);
+  printArray(arr2);
 
-  //print out arr1
-  printf("arr1:\n");
-  for (int i = 0; i < 2; i++){        //rows
-    for(int j = 0; j < 2; j++){       //columns
-      printf("%d ", arr1[i][j]);
-    }
-    printf("\n");
-  }
-
-  //print out arr2
-  printf("arr2:\n");
-  for (int i = 0; i < 2; i++){        //rows
-    for(int j = 0; j < 2; j++){       //columns
-      printf("%d ", arr2[i][j]);
-    }
-    printf("\n");
-  }
-
-  for (int i = 0; i < 2; i++){        //rows
-    for(int j = 0; j < 2; j++){       //columns
-      int temporary = arr1[i][j];
-      arr1[i][j] = arr2[i][j];
-      arr2[i][j] = temporary;
-    }
-  }
+  swapArrays(arr1, arr2);
 
   printf("\nSwapped content of arr1 and arr2:\n");
-
-  //print out arr1
-  printf("arr1:\n");
-  for (int i = 0; i < 2; i++){        //rows
-    for(int j = 0; j < 2; j++){       //columns
-      printf("%d ", arr1[i][j]);
-    }
-    printf("\n");
-  }
-
-  //print out arr2
-  printf("arr2:\n");
-  for (int i = 0; i < 2; i++){        //rows
-    for(int j = 0; j < 2; j++){       //columns
-      printf("%d ", arr2[i][j]);
-    }
-    printf("\n");
-  }
+  printArray(arr1);
+  printArray(arr2);
 
 return 0;
 }
