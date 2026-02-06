8d6ebc157ca546f044cc2148c89baf67595dbc53
Jean-Luc Pischa <pischa.jean-lu-it25@it.dhbw-ravensburg.de>
2026-02-06 12:54:08 +0100

    create a header file for calculator

--- challenges/calculator_section1.c
+++ challenges/calculator_section1.c
@@ -1,21 +1,5 @@
 #include <stdio.h>
-
-float add(float operand1, float operand2){
-  return operand1+operand2;
-}
-float subtract(float operand1, float operand2){
-  return operand1-operand2;
-}
-float multiply(float operand1, float operand2){
-  return operand1*operand2;
-}
-float divide(float operand1, float operand2){
- return operand1/operand2;
-}
-
-void printResult(float operand1, float operand2, char operator, float (*calc)(float, float)){
-  printf("Das Ergebnis der Berechnung ist: %.2f %c %.2f = %.2f", operand1, operator, operand2, calc(operand1, operand2));
-}
+#include "calculator_operations.h"
 
 int main(){
 
