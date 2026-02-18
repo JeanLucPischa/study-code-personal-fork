39d443b01026a8b94d04cc2e2443b616aef26881
Jean-Luc Pischa <pischa.jean-lu-it25@it.dhbw-ravensburg.de>
2026-02-18 21:04:35 +0100

    add mySolution

--- courses/coding-in-C/Lab_6/mySolution/Lab6_Sec1.c
+++ courses/coding-in-C/Lab_6/mySolution/Lab6_Sec1.c
@@ -2,9 +2,28 @@
 
 int main(){
 
-  FILE* data = NULL;
+  FILE* data = fopen("test_file.txt", "r");
 
-  if(data = fopen("sensor.txt", "r")){
+  if(data == NULL){
+    printf("Datei konnte nicht geoeffnet werden.");
+    return 1;
+  }
+
+  /*int c = 0;
+  while((c = fgetc(data)) != EOF){
+    printf("%c", c);  
+  }*/
+
+  char *string = NULL;
+  char *puffer = NULL;
+  while((string = fgets(puffer, 20, data)) != NULL){
+    printf("%s", string);
+  }
+
+  int success = fclose(data);
+
+  if(success){
+    printf("Datei konnte nicht geschlossen werden.");
     return 1;
   }
 
